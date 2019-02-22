6d388609338745f4802fe132720aa9d6d5cd54e0
kwang40 <kaishenw1995@gmail.com>
2019-02-22 14:02:08 -0600

    add part to process zdns output

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os/exec"
 	"fmt"
+	"io"
 )
 
 var (
@@ -20,6 +21,7 @@ var (
 
 	domainToUrl map[string][]string
 	domains strings.Builder
+	ipToDomains map[int32][]string
 )
 
 
@@ -58,13 +60,32 @@ func readURL(wg *sync.WaitGroup) error {
 		if !contains(value, urlStr) {
 			domainToUrl[fqdn] = append(value, urlStr)
 		}
-		//fmt.Println(fqdn)
 		if !ok {
 			domains.WriteString(fqdn + "\n")
 		}
 
 	}
 	return nil
+}
+
+func processZDNSOutput(wg *sync.WaitGroup, reader io.ReadCloser, zmapInput chan<- string) {
+	defer (*wg).Done()
+	defer close(zmapInput)
+	ipToDomains = make(map[int32][]string)
+	rd := bufio.NewReader(reader)
+	for {
+		line, err := rd.ReadString('\n')
+		line = strings.TrimSuffix(line, "\n")
+		if err != nil {
+			return
+		}
+		parts := strings.Split(line, ",")
+		ip := parts[0]
+		domain := parts[1]
+		key := ipStr2Int(ip)
+		ipToDomains[key] = append(ipToDomains[key], domain)
+		zmapInput <- ip+"\n"
+	}
 }
 
 
@@ -81,15 +102,29 @@ func main() {
 	go readURL(&readUrlWG)
 	readUrlWG.Wait()
 	
-	exeZDNS := exec.Command(os.Getenv("GOPATH")+"/src/github.com/kwang40/zdns/zdns/./zdns", "ALOOKUP", "-iterative", "--cache-size=500000", "--std-out-modules=A", "--input-file=tmp.txt", "--output-file="+zdnsOutputFile)
+	exeZDNS := exec.Command(os.Getenv("GOPATH")+"/src/github.com/kwang40/zdns/zdns/./zdns", "ALOOKUP", "-iterative", "--cache-size=500000", "--std-out-modules=A", "--output-file="+zdnsOutputFile)
 	exeZDNS.Stdin = strings.NewReader(domains.String())
-	exeZDNS.Stdout = os.Stdout
-	exeZDNS.Stderr= os.Stderr
-
+	exeZDNSOut,_ := exeZDNS.StdoutPipe()
 	if err := exeZDNS.Start(); err != nil { //Use start, not run
 		fmt.Println("An error occured: ", err) //replace with logger, or anything you want
 	}
 
+	var testWG sync.WaitGroup
+	testWG.Add(1)
+	zmapInput := make(chan string)
+	go testOutput(&testWG, zmapInput)
+
+
+	var processZDNSOutputWG sync.WaitGroup
+	processZDNSOutputWG.Add(1)
+	go processZDNSOutput(&processZDNSOutputWG, exeZDNSOut, zmapInput)
+
+
+	if err := exeZDNS.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	processZDNSOutputWG.Wait()
+	testWG.Wait()
 	return
 }
 
@@ -102,4 +137,18 @@ func contains(arr []string, str string) bool {
 		}
 	}
 	return false
+}
+
+func ipStr2Int(ip string) int32{
+	var n0,n1,n2,n3 int32
+	fmt.Sscanf(ip, "%d.%d.%d.%d", &n0,&n1,&n2,&n3)
+	return (n0<<24)|(n1<<16)|(n2<<8)|(n3)
+
+}
+
+func testOutput(wg *sync.WaitGroup, in <- chan string) {
+	defer wg.Done()
+	for line := range(in) {
+		fmt.Print(line)
+	}
 }
